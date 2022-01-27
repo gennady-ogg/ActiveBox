ae258ed4e93cea3e86b21d4ce2aa3c400e4ee939
gennady-ogg <genn.osipov2011@yandex.ru>
2022-01-27 21:33:13 +0300

    activeBox

---
+++ js/script.js
@@ -0,0 +1,37 @@
+// gamb
+$('.wrapper').addClass('loaded');
+
+$('.icon-menu').click(function(event) {
+	$(this).toggleClass('active');
+	$('.menu__body').toggleClass('active');
+	$('body').toggleClass('lock');
+});
+$('.menu__body a').click(function() {
+
+	$('.menu__body, .icon-menu').removeClass('active');
+	$('body').removeClass('lock');
+
+});
+
+//scroll
+$(function() {
+    // при нажатии на кнопку scrollup
+    $('.scrollup').click(function() {
+      // переместиться в верхнюю часть страницы
+      $("html, body").animate({
+        scrollTop:0
+      },1000);
+    })
+  })
+  // при прокрутке окна (window)
+  $(window).scroll(function() {
+    // если пользователь прокрутил страницу более чем на 200px
+    if ($(this).scrollTop()>200) {
+      // то сделать кнопку scrollup видимой
+      $('.scrollup').fadeIn();
+    }
+    // иначе скрыть кнопку scrollup
+    else {
+      $('.scrollup').fadeOut();
+    }
+  });
